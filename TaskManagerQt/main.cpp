f19d4e698e6c4a0e004fbc3d22c69659c8dc36e1
paaket <122210350+paaket@users.noreply.github.com>
2026-06-22 20:37:57 +0300

    visual changes

--- TaskManagerQt/main.cpp
+++ TaskManagerQt/main.cpp
@@ -3,7 +3,10 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
     TaskManagerQt window;
+    window.setMinimumSize(900, 600);
     window.show();
+
     return app.exec();
 }
