0d6a97678489933c9a2d69f1dbd8455454ef1626
paaket <122210350+paaket@users.noreply.github.com>
2026-06-22 20:39:43 +0300

    feat: add addTask, handAddData slots

--- TaskManagerQt/TaskManagerQt.cpp
+++ TaskManagerQt/TaskManagerQt.cpp
@@ -1,7 +1,54 @@
 #include "TaskManagerQt.h"
 
 TaskManagerQt::TaskManagerQt(QWidget *parent) : QMainWindow(parent) {
-    
+    QWidget* mainWidget = new QWidget(this);
+    setCentralWidget(mainWidget);
+
+    list = new QListWidget();
+
+    QLabel* infoWidget = new QLabel();
+    infoWidget->setFrameShape(QFrame::StyledPanel);
+    infoWidget->setFrameShadow(QFrame::Sunken);
+    infoWidget->setLineWidth(1);
+
+    QPushButton* addBtn = new QPushButton("add task");
+    QPushButton* deleteBtn = new QPushButton("delete task");
+    QPushButton* editBtn = new QPushButton("edit a task");
+    QPushButton* tempBtn = new QPushButton("temp btn");
+    addBtn->setMinimumSize(130, 40);
+    deleteBtn->setMinimumSize(130, 40);
+    editBtn->setMinimumSize(130, 40);
+    tempBtn->setMinimumSize(130, 40);
+
+    QGridLayout* grid = new QGridLayout();
+    grid->addWidget(addBtn, 0, 0);
+    grid->addWidget(deleteBtn, 0, 1);
+    grid->addWidget(editBtn, 1, 0);
+    grid->addWidget(tempBtn, 1, 1);
+
+    QVBoxLayout* vbox = new QVBoxLayout();
+    vbox->addWidget(infoWidget);
+    vbox->addItem(grid);
+
+    QHBoxLayout* hbox = new QHBoxLayout();
+    hbox->addWidget(list);
+    hbox->addLayout(vbox);
+
+    connect(addBtn, &QPushButton::clicked, this, &TaskManagerQt::addTask);
+
+    mainWidget->setLayout(hbox);
+
+    statusBar()->showMessage("started", 3000);
+}
+
+void TaskManagerQt::addTask() {
+    CreateTaskWindow window(this);
+    connect(&window, &CreateTaskWindow::saveReady, this, &TaskManagerQt::handAddData);
+    window.exec();
+}
+
+void TaskManagerQt::handAddData(const QVector<QString>& data) {
+
 }
 
 TaskManagerQt::~TaskManagerQt() {
--- TaskManagerQt/TaskManagerQt.h
+++ TaskManagerQt/TaskManagerQt.h
@@ -1,9 +1,26 @@
 #pragma once
+#include "CreateTaskWindow.h"
 #include <QMainWindow>
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QListWidget>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QStatusBar>
+#include <QFrame>
+#include <QLabel>
+#include <QString>
+#include <QVector>
 
 class TaskManagerQt : public QMainWindow {
     Q_OBJECT
 public:
     TaskManagerQt(QWidget *parent = nullptr);
     ~TaskManagerQt();
+private slots:
+    void addTask();
+    void handAddData(const QVector<QString>& data);
+private:
+    QListWidget* list;
 };
