51da31cd66257ec322d11728ab3f137757e71de9
paaket <122210350+paaket@users.noreply.github.com>
2026-06-27 12:10:29 +0300

    refactor: add DatabaseManager

--- TaskManagerQt/main.cpp
+++ TaskManagerQt/main.cpp
@@ -1,3 +1,4 @@
+#include "DatabaseManager.h"
 #include "LoginWindow.h"
 #include "TaskManagerQt.h"
 #include <QApplication>
@@ -8,17 +9,11 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("taskManager.db");
-    if (!db.open()) return 0;
-
-    QSqlQuery query;
-    QString queryText;
-    queryText = "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, login TEXT UNIQUE NOT NULL, password TEXT NOT NULL);";
-    if (!query.exec(queryText)) return 0;
-
-    queryText = "CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER NOT NULL, title TEXT NOT NULL, description TEXT NOT NULL, priority INTEGER NOT NULL, deadline TEXT NOT NULL, completed INTEGER NOT NULL, created_at TEXT NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id));";
-    if (!query.exec(queryText)) return 0;
+    DatabaseManager databaseManager;
+    if (!databaseManager.open()) return 0;
+    
+    if (!databaseManager.createUsersDatabase()) return 0;
+    if (!databaseManager.createTasksDatabase()) return 0;
 
     QSettings settings("Paket", "TaskManagerQt");
     if (!settings.contains("currentUserId")) {
