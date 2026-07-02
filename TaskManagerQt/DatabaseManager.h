09435ede4c2b2586b28e7ec8a8afa887e46933c8
paaket <122210350+paaket@users.noreply.github.com>
2026-07-02 20:51:31 +0300

    refactor: transfer work with sql from other files

--- TaskManagerQt/DatabaseManager.h
+++ TaskManagerQt/DatabaseManager.h
@@ -1,13 +1,26 @@
 #pragma once
 #include <QSqlDatabase>
 #include <QSqlQuery>
+#include <QSqlError>
 #include <QString>
+#include <QSettings>
+#include <QVector>
+#include "Task.h"
+#include "CreateTaskWindow.h"
 
 class DatabaseManager {
 public:
 	bool open();
 	bool createUsersDatabase();
 	bool createTasksDatabase();
+	bool checkLogin(const QString& login);
+	QString addNewUser(const QString& login, const QString& password);
+	QString logInToAccount(const QString& login, const QString& password, bool remember);
+	QVector<Task> findTasksById(int userId);
+	QString deleteTaskBtId(int id);
+	QString updateTask(const CreateTaskWindow::TaskData& data, int id);
+	QString markTaskCompleted(int id, int newState);
+	QString createTask(const CreateTaskWindow::TaskData& data, int userId);
 	~DatabaseManager();
 private:
 	QSqlDatabase db;
