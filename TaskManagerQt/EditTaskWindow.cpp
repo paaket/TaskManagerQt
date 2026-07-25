37be6cb22cfa33656dc1c473ae019abf3cffd09d
paaket <122210350+paaket@users.noreply.github.com>
2026-07-25 18:15:10 +0300

    feat: add the ability to change the task folder

--- TaskManagerQt/EditTaskWindow.cpp
+++ TaskManagerQt/EditTaskWindow.cpp
@@ -1,8 +1,53 @@
 #include "EditTaskWindow.h"
 
-EditTaskWindow::EditTaskWindow(QWidget* parent, const QList<QString>& list) : CreateTaskWindow(parent) {
+EditTaskWindow::EditTaskWindow(QWidget* parent, const QList<QString>& list, const QVector<Folder>& folders) : CreateTaskWindow(parent) {
+	this->folders = folders;
+	taskId = list[4].toInt();
+	folderId = list[5].toInt();
+
 	title->setText(list[0]);
 	priority->setCurrentIndex(list[2].toInt());
 	deadline->setDate(QDate::fromString(list[3], "dd.MM.yyyy"));
 	description->setText(list[1]);
+
+	QPushButton* deleteBtn = new QPushButton("Delete task", this);
+	btnGrid->addWidget(deleteBtn, 0, 0, 1, 2);
+
+	QLabel* folderLbl = new QLabel("Folder:", this);
+	folderBox = new QComboBox(this);
+	for (auto folder : folders) folderBox->addItem(folder.title);
+	for (int i = 0; i < folders.size(); i++) if (folders[i].id == list[5].toInt()) folderBox->setCurrentIndex(i);
+
+	grid->addWidget(folderLbl, 4, 0);
+	grid->addWidget(folderBox, 4, 1);
+
+	connect(deleteBtn, &QPushButton::clicked, this, &EditTaskWindow::deleteClicked);
+	connect(folderBox, &QComboBox::currentTextChanged, this, &EditTaskWindow::folderChanged);
+}
+
+void EditTaskWindow::deleteClicked() {
+	QString msg = "Delete selected task?\nThe action cannot be undone";
+	int ret = QMessageBox::question(this, "delete task", msg, QMessageBox::Yes | QMessageBox::No);
+	if (ret == QMessageBox::Yes) {
+		emit deleteReady(taskId);
+		accept();
+	}
+}
+
+void EditTaskWindow::folderChanged(const QString& text) {
+	for (auto folder : folders)
+		if (folder.title == text)
+			folderId = folder.id;
+}
+
+void EditTaskWindow::saveClicked() {
+	if (title->text().simplified() == "" or description->toPlainText().simplified() == ""
+		or priority->currentIndex() == 0 or deadline->text().simplified() == "") {
+		QMessageBox::warning(this, "error", "fill in all fields");
+		return;
+	}
+	TaskData task = { title->text(), description->toPlainText(), priority->currentIndex(), deadline->text(), QDate::currentDate().toString("dd.MM.yyyy") };
+	emit saveReady(task);
+	emit folderChangeReady(taskId, folderId);
+	accept();
 }
--- TaskManagerQt/EditTaskWindow.h
+++ TaskManagerQt/EditTaskWindow.h
@@ -1,10 +1,26 @@
 #pragma once
-#include "CreateTaskWindow.h"
 #include <QWidget>
 #include <QList>
+#include <QComboBox>
+#include <QLabel>
+#include "CreateTaskWindow.h"
+#include "Folder.h"
+#include "DatabaseManager.h"
 
 class EditTaskWindow : public CreateTaskWindow {
 	Q_OBJECT
 public:
-	EditTaskWindow(QWidget* parent = nullptr, const QList<QString>& list = {});
+	EditTaskWindow(QWidget* parent = nullptr, const QList<QString>& list = {}, const QVector<Folder>& folders = {});
+private slots:
+	void deleteClicked();
+	void folderChanged(const QString& text);
+	void saveClicked() override;
+signals:
+	void deleteReady(int iaskId);
+	void folderChangeReady(int taskId, int folderId);
+private:
+	QComboBox* folderBox;
+	int taskId;
+	int folderId;
+	QVector<Folder> folders;
 };
