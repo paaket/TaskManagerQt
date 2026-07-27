8845be8ba95cf8449f846b54f6bacadfd73e7cd4
paaket <122210350+paaket@users.noreply.github.com>
2026-07-27 21:23:08 +0300

    add new databases

--- TaskManagerQt/main.cpp
+++ TaskManagerQt/main.cpp
@@ -15,6 +15,8 @@ int main(int argc, char *argv[]) {
     if (!databaseManager.createUsersDatabase()) return 0;
     if (!databaseManager.createFoldersDatabase()) return 0;
     if (!databaseManager.createTasksDatabase()) return 0;
+    if (!databaseManager.createTelegramAccountsDatabase()) return 0;
+    if (!databaseManager.createTelegramLinkCodes()) return 0;
 
     QSettings settings("Paket", "TaskManagerQt");
     if (!settings.contains("currentUserId")) {
